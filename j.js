bad08664116f0a2a07846fa06960e20a4df348fc
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-02-27 21:30:47 -0500

    almost no semicolons allowed

--- j.js
+++ j.js
@@ -1,7 +1,7 @@
-let x=document.querySelector('form'), s=0;
+let x=document.querySelector('form'), s=0
 for(let i=0; i<10; i++) {x.innerHTML+=`<p id='p${i}' style="">${i}?</p><input type='text' id='${i}'>`}
 x.addEventListener('submit', e=> {
-    e.preventDefault();
+    e.preventDefault()
     for(let i=0; i<10; i++) {if(document.getElementById(`${i}`).value==i){document.getElementById(`p${i}`).style.backgroundColor="green";s++}
     else{document.getElementById(`p${i}`).style.backgroundColor="red"}}
     x.innerHTML+=s})
